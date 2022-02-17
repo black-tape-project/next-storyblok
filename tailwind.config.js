44d427d8947ec214022be4c76fe84546f80ddd39
Stephen Wilde <kink@mandys.house>
2022-02-17 12:33:33 +0000

    Add tailwindcss

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+    content: ["./components/**/*.{js,jsx}", "./pages/**/*.{js,jsx}"],
+    theme: {
+        container: {
+            center: true,
+            padding: "1rem",
+        },
+        extend: {},
+    },
+    plugins: [],
+};
