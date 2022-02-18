ac73a0192e10df581583e4e2bc76dbc9108aae5c
Stephen Wilde <kink@mandys.house>
2022-02-18 18:34:24 +0000

    Update code component style

--- components/atoms/code/index.js
+++ components/atoms/code/index.js
@@ -3,7 +3,7 @@ import { FiX, FiMinus, FiMaximize2 } from "react-icons/fi";
 export default function AtomsCode({ content }) {
     return (
         <div className="mb-4 shadow-2xl">
-            <div className="flex py-3 px-4 bg-gray-900 rounded-t-lg">
+            <div className="flex py-3 px-4 bg-gray-800 rounded-t-lg border border-b border-gray-900">
                 <div className="flex justify-center items-center mr-2 min-w-[1rem] min-h-[1rem] bg-red-500 rounded-full">
                     <FiX className="text-xs" />
                 </div>
@@ -14,7 +14,7 @@ export default function AtomsCode({ content }) {
                     <FiMaximize2 className="text-xs" />
                 </div>
             </div>
-            <code className="block overflow-scroll p-4 max-h-80 text-base text-white bg-gray-800 rounded-b-lg">
+            <code className="block overflow-scroll p-4 max-h-80 text-base text-white bg-gray-700 rounded-b-lg shadow-inner">
                 <pre>{JSON.stringify(content, null, 2)}</pre>
             </code>
         </div>
