7326a62e40c6f8b040b6dab7d1df283b8d582a98
Stephen Wilde <kink@mandys.house>
2022-02-18 18:25:09 +0000

    Add react-icons

--- components/atoms/code/index.js
+++ components/atoms/code/index.js
@@ -1,10 +1,18 @@
+import { FiX, FiMinus, FiMaximize2 } from "react-icons/fi";
+
 export default function AtomsCode({ content }) {
     return (
         <div className="mb-4 shadow-2xl">
             <div className="flex py-3 px-4 bg-gray-900 rounded-t-lg">
-                <div className="mr-2 w-3 h-3 bg-red-500 rounded-full"></div>
-                <div className="mr-2 w-3 h-3 bg-yellow-500 rounded-full"></div>
-                <div className="w-3 h-3 bg-green-500 rounded-full"></div>
+                <div className="flex justify-center items-center mr-2 min-w-[1rem] min-h-[1rem] bg-red-500 rounded-full">
+                    <FiX className="text-xs" />
+                </div>
+                <div className="flex justify-center items-center mr-2 min-w-[1rem] min-h-[1rem] bg-yellow-500 rounded-full">
+                    <FiMinus className="text-xs" />
+                </div>
+                <div className="flex justify-center items-center min-w-[1rem] min-h-[1rem] bg-green-500 rounded-full">
+                    <FiMaximize2 className="text-xs" />
+                </div>
             </div>
             <code className="block overflow-scroll p-4 max-h-80 text-base text-white bg-gray-800 rounded-b-lg">
                 <pre>{JSON.stringify(content, null, 2)}</pre>
